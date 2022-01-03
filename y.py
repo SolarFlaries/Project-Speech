c2a02168d45345cea6275a18682dadf5a424c108
SolarFlaries <jlan@buffalo.edu>
2022-01-03 04:21:18 -0500

    test

---
+++ y.py
@@ -0,0 +1,75 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import requests
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say("Hi I am Andreana")
+engine.say("How can I help you today")
+engine.runAndWait()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    
+    except:
+        pass
+    return command
+
+def join(token, server):
+            header = {"authorization": token}
+            requests.post("https://discord.com/api/v8/invites/{}".format(server), headers=header)
+
+def run_alexa():    
+    command = take_command()
+    print (command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+        engine.stop()
+    
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    
+    elif 'discord' in command:
+        talk("okay, joining ")
+        join("MTUzMzMxNTgzMTU4MzIxMTUy", "87TGwCjJpG")
+    elif 'date' in command:
+        date = datetime.datetime.now().strftime('%A'+ "The " +':%e' + "of "+ '%B')
+        talk('Today is '+ date)
+    
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
